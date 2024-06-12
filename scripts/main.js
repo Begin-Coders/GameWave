a723cbb2e0a9c29fabbaf3d1487a1b7888e90c28
GoAmeer030 <ameer03062003@gmail.com>
2024-06-12 12:13:09 +0530

    color scheme and theme setup

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
-console.log("Welcome to GameWave!")
+console.log("Welcome to GameWave!");
+
+// Default theme
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+prefersDarkScheme.addEventListener("change", (e) => {
+    const root = document.documentElement;
+
+    if (e.matches) {
+        root.classList.add("dark");
+    } else {
+        root.classList.remove("dark");
+    }
+});
